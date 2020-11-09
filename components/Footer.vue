cfab11066de7adb37f7cb18be4b4fa2fd7449a36
irakli.tavberidze <irakli.tavberidze@webfg.com>
2020-11-09 13:19:57 +0100

    css

---
+++ components/Footer.vue
@@ -0,0 +1,137 @@
+<template>
+<footer class="footer-v5 padding-y-lg">
+  <div class="container max-width-lg">
+    <!-- 👇 footer intro -->
+    <div class="border-bottom padding-bottom-xs text-sm@md flex@md justify-between@md items-center@md">
+      <h4 class="margin-bottom-xs hide@md">Popular links</h4>
+      <nav>
+        <ul class="flex flex-wrap gap-xxs">
+          <li><a class="footer-v5__popular-link" href="#0">Pricing</a></li>
+          <li><a class="footer-v5__popular-link" href="#0">Teams</a></li>
+          <li><a class="footer-v5__popular-link" href="#0">Updates</a></li>
+          <li><a class="footer-v5__popular-link" href="#0">Features</a></li>
+          <li><a class="footer-v5__popular-link" href="#0">Integrations</a></li>
+        </ul>
+      </nav>
+
+      <a href="#" class="footer-v5__back-to-top display@md">Back to top &uarr;</a>
+    </div>
+
+    <!-- 👇 footer body -->
+    <div class="padding-y-xl grid gap-md">
+      <div class="col-8@lg">
+        <div class="grid gap-md">
+          <div class="col-6@xs col-3@md">
+            <h4 class="margin-bottom-xs text-base@md">Product</h4>
+            <ul class="grid gap-xs text-sm@md">
+              <li><a class="footer-v5__link" href="#0">Pricing</a></li>
+              <li><a class="footer-v5__link" href="#0">Teams</a></li>
+              <li><a class="footer-v5__link" href="#0">Updates</a></li>
+              <li><a class="footer-v5__link" href="#0">Features</a></li>
+              <li><a class="footer-v5__link" href="#0">Integrations</a></li>
+              <li><a class="footer-v5__link" href="#0">Support</a></li>
+            </ul>
+          </div>
+
+          <div class="col-6@xs col-3@md">
+            <h4 class="margin-bottom-xs text-base@md">Developers</h4>
+            <ul class="grid gap-xs text-sm@md">
+              <li><a class="footer-v5__link" href="#0">Documentation</a></li>
+              <li><a class="footer-v5__link" href="#0">API reference</a></li>
+              <li><a class="footer-v5__link" href="#0">API status</a></li>
+              <li><a class="footer-v5__link" href="#0">Open source</a></li>
+            </ul>
+          </div>
+
+          <div class="col-6@xs col-3@md">
+            <h4 class="margin-bottom-xs text-base@md">Resources</h4>
+            <ul class="grid gap-xs text-sm@md">
+              <li><a class="footer-v5__link" href="#0">Tutorials</a></li>
+              <li><a class="footer-v5__link" href="#0">Docs</a></li>
+              <li><a class="footer-v5__link" href="#0">Community</a></li>
+              <li><a class="footer-v5__link" href="#0">Case studies</a></li>
+              <li><a class="footer-v5__link" href="#0">Help center</a></li>
+            </ul>
+          </div>
+
+          <div class="col-6@xs col-3@md">
+            <h4 class="margin-bottom-xs text-base@md">About</h4>
+            <ul class="grid gap-xs text-sm@md">
+              <li><a class="footer-v5__link" href="#0">Company</a></li>
+              <li><a class="footer-v5__link" href="#0">Customers</a></li>
+              <li><a class="footer-v5__link" href="#0">Careers</a></li>
+              <li><a class="footer-v5__link" href="#0">Education</a></li>
+              <li><a class="footer-v5__link" href="#0">Our story</a></li>
+              <li><a class="footer-v5__link" href="#0">Press kit</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-4@lg">
+        <div class="max-width-xxs">
+          <div class="text-component margin-bottom-sm">
+            <h4 class="text-base@md">Subscribe to our newsletter</h4>
+            <p class="text-sm color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nesciunt nostrum numquam.</p>
+          </div>
+
+          <form class="grid gap-xxxs text-sm@md">
+            <input class="form-control col min-width-0" type="email" placeholder="Your email address" aria-label="Email address">
+            <button class="btn btn--primary col-content">Subscribe</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    
+    <!-- 👇 footer colophon -->
+    <div class="border-top padding-top-sm">
+      <div class="flex flex-column items-center gap-sm flex-row@md justify-between@md">
+        <div>
+          <p class="text-sm color-contrast-medium text-xs@md">&copy; Copyright CompanyX</p>
+        </div>
+  
+        <div>
+          <div class="flex flex-wrap gap-xxs text-sm@md">
+            <a class="footer-v5__social-btn" href="#0">
+              <svg class="icon" viewBox="0 0 16 16"><title>Follow us on Twitter</title><g><path d="M16,3c-0.6,0.3-1.2,0.4-1.9,0.5c0.7-0.4,1.2-1,1.4-1.8c-0.6,0.4-1.3,0.6-2.1,0.8c-0.6-0.6-1.5-1-2.4-1 C9.3,1.5,7.8,3,7.8,4.8c0,0.3,0,0.5,0.1,0.7C5.2,5.4,2.7,4.1,1.1,2.1c-0.3,0.5-0.4,1-0.4,1.7c0,1.1,0.6,2.1,1.5,2.7 c-0.5,0-1-0.2-1.5-0.4c0,0,0,0,0,0c0,1.6,1.1,2.9,2.6,3.2C3,9.4,2.7,9.4,2.4,9.4c-0.2,0-0.4,0-0.6-0.1c0.4,1.3,1.6,2.3,3.1,2.3 c-1.1,0.9-2.5,1.4-4.1,1.4c-0.3,0-0.5,0-0.8,0c1.5,0.9,3.2,1.5,5,1.5c6,0,9.3-5,9.3-9.3c0-0.1,0-0.3,0-0.4C15,4.3,15.6,3.7,16,3z"></path></g></svg>
+            </a>
+      
+            <a class="footer-v5__social-btn" href="#0">
+              <svg class="icon" viewBox="0 0 16 16"><title>Follow us on Youtube</title><g><path d="M15.8,4.8c-0.2-1.3-0.8-2.2-2.2-2.4C11.4,2,8,2,8,2S4.6,2,2.4,2.4C1,2.6,0.3,3.5,0.2,4.8C0,6.1,0,8,0,8 s0,1.9,0.2,3.2c0.2,1.3,0.8,2.2,2.2,2.4C4.6,14,8,14,8,14s3.4,0,5.6-0.4c1.4-0.3,2-1.1,2.2-2.4C16,9.9,16,8,16,8S16,6.1,15.8,4.8z M6,11V5l5,3L6,11z"></path></g></svg>
+            </a>
+      
+            <a class="footer-v5__social-btn" href="#0">
+              <svg class="icon" viewBox="0 0 16 16"><title>Follow us on Github</title><g><path  d="M8,0.2c-4.4,0-8,3.6-8,8c0,3.5,2.3,6.5,5.5,7.6 C5.9,15.9,6,15.6,6,15.4c0-0.2,0-0.7,0-1.4C3.8,14.5,3.3,13,3.3,13c-0.4-0.9-0.9-1.2-0.9-1.2c-0.7-0.5,0.1-0.5,0.1-0.5 c0.8,0.1,1.2,0.8,1.2,0.8C4.4,13.4,5.6,13,6,12.8c0.1-0.5,0.3-0.9,0.5-1.1c-1.8-0.2-3.6-0.9-3.6-4c0-0.9,0.3-1.6,0.8-2.1 c-0.1-0.2-0.4-1,0.1-2.1c0,0,0.7-0.2,2.2,0.8c0.6-0.2,1.3-0.3,2-0.3c0.7,0,1.4,0.1,2,0.3c1.5-1,2.2-0.8,2.2-0.8 c0.4,1.1,0.2,1.9,0.1,2.1c0.5,0.6,0.8,1.3,0.8,2.1c0,3.1-1.9,3.7-3.7,3.9C9.7,12,10,12.5,10,13.2c0,1.1,0,1.9,0,2.2 c0,0.2,0.1,0.5,0.6,0.4c3.2-1.1,5.5-4.1,5.5-7.6C16,3.8,12.4,0.2,8,0.2z"></path></g></svg>
+            </a>
+          </div>
+        </div>
+  
+        <div>
+          <p class="text-sm color-contrast-medium flex flex-wrap gap-xs text-xs@md">
+            <a class="color-inherit text-underline" href="#0">Terms</a>
+            <a class="color-inherit text-underline" href="#0">Privacy</a>
+            <a class="color-inherit text-underline" href="#0">Cookies</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+</template>
+
+<script>
+
+export default {
+  mounted(){
+  
+  },
+  destroyed() { // remove the JS code once the component has been destroyed
+      
+    }
+}
+</script>
+<style lang="scss">
+
+</style>
+
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,260 +1,151 @@
 <template>
-  <div class="container  max-width-adaptive-md">
-   <div class="grid-switch js-grid-switch" data-gs-item-class-1="col-12 col-4@md" data-gs-item-class-2="col-12" data-gs-content-class-1="card-v10--state-1" data-gs-content-class-2="card-v10--state-2">
-  <div class="margin-bottom-sm text-right display@md no-js:is-hidden">
-    <div class="btns btns--radio js-grid-switch__controller">
-      <div>
-        <input type="radio" name="radio-view-option" id="radio-grid" value="1" checked>
-        <label class="btns__btn btns__btn--icon" for="radio-grid">
-          <svg class="icon icon--xs" viewBox="0 0 16 16">
-            <title>Grid</title>
-            <g>
-              <path d="M6,0H1C0.4,0,0,0.4,0,1v5c0,0.6,0.4,1,1,1h5c0.6,0,1-0.4,1-1V1C7,0.4,6.6,0,6,0z"></path>
-              <path d="M15,0h-5C9.4,0,9,0.4,9,1v5c0,0.6,0.4,1,1,1h5c0.6,0,1-0.4,1-1V1C16,0.4,15.6,0,15,0z"></path>
-              <path d="M6,9H1c-0.6,0-1,0.4-1,1v5c0,0.6,0.4,1,1,1h5c0.6,0,1-0.4,1-1v-5C7,9.4,6.6,9,6,9z"></path>
-              <path d="M15,9h-5c-0.6,0-1,0.4-1,1v5c0,0.6,0.4,1,1,1h5c0.6,0,1-0.4,1-1v-5C16,9.4,15.6,9,15,9z"></path>
-            </g>
-          </svg>
-        </label>
-      </div>
+<div class="app">
+     <Header/>
 
-      <div>
-        <input type="radio" name="radio-view-option" id="radio-list" value="2">
-        <label class="btns__btn btns__btn--icon" for="radio-list">
-          <svg class="icon icon--xs" viewBox="0 0 16 16">
-            <title>List</title>
-            <g>
-              <rect width="16" height="3"></rect>
-              <rect y="6" width="16" height="3"></rect>
-              <rect y="12" width="16" height="3"></rect>
-            </g>
-          </svg>
-        </label>
-      </div>
-    </div>
-  </div>
+  <div class="container  max-width-adaptive-lg">
 
-  <ul class="grid gap-md">
-    <!-- AQUI -->
-    <li v-for="(item, key) in data" :key="key">
-      {{ item.name }}
-    </li>
 
-    <li class="js-grid-switch__item">
 
-      <div class="card-v10 card-v10--state-1 height-100% js-grid-switch__content">
-        <a class="card-v10__img-link radius-lg shadow-lg" href="#0">
-          <img src="https://picsum.photos/300/200" alt="Image description">
-        </a>
+  <div class="main-products-box tabs js-tabs">
+    <Aside />
 
-        <div class="card-v10__content-wrapper">
-          <div class="card-v10__content bg shadow-xs radius-lg">
-            <div class="card-v10__body">
-              <p class="card-v10__label text-uppercase color-primary letter-spacing-md">Category</p>
+<div class="js-tabs__panels main-product-content">
+   <section id="monPanel" class="padding-top-md js-tabs__panel">
+      <ul class="grid gap-md product-grid">
+        <!-- AQUI -->
 
-              <div class="text-component">
-                <h1 class="card-v10__title"><a class="color-contrast-higher" href="#0">The quick brown fox jumps over the lazy dog</a></h1>
-                <p class="card-v10__excerpt color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <li class="col-4@md" v-for="(item,index, key) in products" :key="key">
+
+          <div class="card-v10 card-v10--state-1 height-100%">
+            <a class="card-v10__img-link radius-lg shadow-lg" href="#0">
+              <img src="https://picsum.photos/300/200" alt="Image description">
+            </a>
+
+            <div class="card-v10__content-wrapper">
+              <div class="card-v10__content bg shadow-xs radius-lg">
+                <div class="card-v10__body">
+                  <p class="card-v10__label text-uppercase color-primary letter-spacing-md">{{ item.price + ' €' }}</p>
+
+                  <div class="text-component">
+                    <h1 class="card-v10__title"><a class="color-contrast-higher" href="#0"> {{ item.name }}</a></h1>
+                    <p class="card-v10__excerpt color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                  </div>
+                </div>
+
+                <footer class="card-v10__footer">
+                  <ul class="card-v10__social-list">
+                    <li class="card-v10__social-item">
+                      <a class="reset card-v10__social-btn radius-md" aria-label="rest product" @click="restarProducto()">
+                       <svg class="icon" width="24" height="24" viewBox="0 0 24 24"><path d="M5 11H19V13H5z"/></svg>
+
+                 
+                      </a>
+                    </li>
+
+                    <li class="card-v10__social-item">
+
+                        <span class="quanty">{{catidadProducto}}</span>
+                     
+                    </li>
+
+                    <li class="card-v10__social-item">
+                      <a class="reset card-v10__social-btn radius-md" aria-label="add more product" @click="sumarProducto()">
+                        <svg class="icon" width="24" height="24" viewBox="0 0 24 24"><path d="M19 11L13 11 13 5 11 5 11 11 5 11 5 13 11 13 11 19 13 19 13 13 19 13z"/></svg>
+
+                 
+                      </a>
+                    </li>
+                  </ul>
+                </footer>
               </div>
             </div>
-
-            <footer class="card-v10__footer">
-              <ul class="card-v10__social-list">
-                <li class="card-v10__social-item">
-                  <button class="reset card-v10__social-btn radius-md js-tab-focus" aria-label="Like this content along with 120 other people">
-                    <svg class="icon" viewBox="0 0 12 12">
-                      <g>
-                        <path d="M11.045,2.011a3.345,3.345,0,0,0-4.792,0c-.075.075-.15.225-.225.3-.075-.074-.15-.224-.225-.3a3.345,3.345,0,0,0-4.792,0,3.345,3.345,0,0,0,0,4.792l5.017,4.718L11.045,6.8A3.484,3.484,0,0,0,11.045,2.011Z"></path>
-                      </g>
-                    </svg>
-
-                    <span>120</span>
-                  </button>
-                </li>
-
-                <li class="card-v10__social-item">
-                  <button class="reset card-v10__social-btn radius-md js-tab-focus" aria-label="Comment">
-                    <svg class="icon" viewBox="0 0 12 12">
-                      <g>
-                        <path d="M6,0C2.691,0,0,2.362,0,5.267s2.691,5.266,6,5.266a6.8,6.8,0,0,0,1.036-.079l2.725,1.485A.505.505,0,0,0,10,12a.5.5,0,0,0,.5-.5V8.711A4.893,4.893,0,0,0,12,5.267C12,2.362,9.309,0,6,0Z"></path>
-                      </g>
-                    </svg>
-
-                    <span>Comment</span>
-                  </button>
-                </li>
-
-                <li class="card-v10__social-item">
-                  <button class="reset card-v10__social-btn radius-md js-tab-focus" aria-label="Share">
-                    <svg class="icon" viewBox="0 0 12 12">
-                      <g>
-                        <path d="M6,4C2.975,4,0,5.8,0,11,1.575,8.45,3.6,8,6,8v3l6-5L6,1Z"></path>
-                      </g>
-                    </svg>
-
-                    <span>Share</span>
-                  </button>
-                </li>
-              </ul>
-            </footer>
           </div>
-        </div>
-      </div>
-    </li>
+        </li>
 
-    <li class="js-grid-switch__item">
-
-      <div class="card-v10 card-v10--state-1 height-100% js-grid-switch__content">
-        <a class="card-v10__img-link radius-lg shadow-lg" href="#0">
-          <img src="https://picsum.photos/300/200" alt="Image description">
-        </a>
-
-        <div class="card-v10__content-wrapper">
-          <div class="card-v10__content bg shadow-xs radius-lg">
-            <div class="card-v10__body">
-              <p class="card-v10__label text-uppercase color-primary letter-spacing-md">Category</p>
-
-              <div class="text-component">
-                <h1 class="card-v10__title"><a class="color-contrast-higher" href="#0">Almost before we knew it, we had left the ground</a></h1>
-                <p class="card-v10__excerpt color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </div>
-            </div>
-
-            <footer class="card-v10__footer">
-              <ul class="card-v10__social-list">
-                <li class="card-v10__social-item">
-                  <button class="reset card-v10__social-btn radius-md js-tab-focus" aria-label="Like this content along with 140 other people">
-                    <svg class="icon" viewBox="0 0 12 12">
-                      <g>
-                        <path d="M11.045,2.011a3.345,3.345,0,0,0-4.792,0c-.075.075-.15.225-.225.3-.075-.074-.15-.224-.225-.3a3.345,3.345,0,0,0-4.792,0,3.345,3.345,0,0,0,0,4.792l5.017,4.718L11.045,6.8A3.484,3.484,0,0,0,11.045,2.011Z"></path>
-                      </g>
-                    </svg>
-
-                    <span>140</span>
-                  </button>
-                </li>
-
-                <li class="card-v10__social-item">
-                  <button class="reset card-v10__social-btn radius-md js-tab-focus" aria-label="Comment">
-                    <svg class="icon" viewBox="0 0 12 12">
-                      <g>
-                        <path d="M6,0C2.691,0,0,2.362,0,5.267s2.691,5.266,6,5.266a6.8,6.8,0,0,0,1.036-.079l2.725,1.485A.505.505,0,0,0,10,12a.5.5,0,0,0,.5-.5V8.711A4.893,4.893,0,0,0,12,5.267C12,2.362,9.309,0,6,0Z"></path>
-                      </g>
-                    </svg>
-
-                    <span>Comment</span>
-                  </button>
-                </li>
-
-                <li class="card-v10__social-item">
-                  <button class="reset card-v10__social-btn radius-md js-tab-focus" aria-label="Share">
-                    <svg class="icon" viewBox="0 0 12 12">
-                      <g>
-                        <path d="M6,4C2.975,4,0,5.8,0,11,1.575,8.45,3.6,8,6,8v3l6-5L6,1Z"></path>
-                      </g>
-                    </svg>
-
-                    <span>Share</span>
-                  </button>
-                </li>
-              </ul>
-            </footer>
-          </div>
-        </div>
-      </div>
-    </li>
-
-    <li class="js-grid-switch__item">
-
-      <div class="card-v10 card-v10--state-1 height-100% js-grid-switch__content">
-        <a class="card-v10__img-link radius-lg shadow-lg" href="#0">
-          <img src="https://picsum.photos/300/200" alt="Image description">
-        </a>
-
-        <div class="card-v10__content-wrapper">
-          <div class="card-v10__content bg shadow-xs radius-lg">
-            <div class="card-v10__body">
-              <p class="card-v10__label text-uppercase color-primary letter-spacing-md">Category</p>
-
-              <div class="text-component">
-                <h1 class="card-v10__title"><a class="color-contrast-higher" href="#0">The spectacle before us was indeed sublime</a></h1>
-                <p class="card-v10__excerpt color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </div>
-            </div>
-
-            <footer class="card-v10__footer">
-              <ul class="card-v10__social-list">
-                <li class="card-v10__social-item">
-                  <button class="reset card-v10__social-btn radius-md js-tab-focus" aria-label="Like this content along with 12K other people">
-                    <svg class="icon" viewBox="0 0 12 12">
-                      <g>
-                        <path d="M11.045,2.011a3.345,3.345,0,0,0-4.792,0c-.075.075-.15.225-.225.3-.075-.074-.15-.224-.225-.3a3.345,3.345,0,0,0-4.792,0,3.345,3.345,0,0,0,0,4.792l5.017,4.718L11.045,6.8A3.484,3.484,0,0,0,11.045,2.011Z"></path>
-                      </g>
-                    </svg>
-
-                    <span>12K</span>
-                  </button>
-                </li>
-
-                <li class="card-v10__social-item">
-                  <button class="reset card-v10__social-btn radius-md js-tab-focus" aria-label="Comment">
-                    <svg class="icon" viewBox="0 0 12 12">
-                      <g>
-                        <path d="M6,0C2.691,0,0,2.362,0,5.267s2.691,5.266,6,5.266a6.8,6.8,0,0,0,1.036-.079l2.725,1.485A.505.505,0,0,0,10,12a.5.5,0,0,0,.5-.5V8.711A4.893,4.893,0,0,0,12,5.267C12,2.362,9.309,0,6,0Z"></path>
-                      </g>
-                    </svg>
-
-                    <span>Comment</span>
-                  </button>
-                </li>
-
-                <li class="card-v10__social-item">
-                  <button class="reset card-v10__social-btn radius-md js-tab-focus" aria-label="Share">
-                    <svg class="icon" viewBox="0 0 12 12">
-                      <g>
-                        <path d="M6,4C2.975,4,0,5.8,0,11,1.575,8.45,3.6,8,6,8v3l6-5L6,1Z"></path>
-                      </g>
-                    </svg>
-
-                    <span>Share</span>
-                  </button>
-                </li>
-              </ul>
-            </footer>
-          </div>
-        </div>
-      </div>
-    </li>
-  </ul>
+      </ul>
+   </section>
+    <section id="thuPanel" class="padding-top-md js-tabs__panel"></section>
+    <section id="wedPanel" class="padding-top-md js-tabs__panel"></section>
+    <section id="tuePanel" class="padding-top-md js-tabs__panel"></section>
+    <section id="friPanel" class="padding-top-md js-tabs__panel"></section>
+    <section id="satPanel" class="padding-top-md js-tabs__panel"></section>
+    <section id="sunPanel" class="padding-top-md js-tabs__panel"></section>
 </div>
+    </div>
+
+  </div>
+      <Footer/>
   </div>
 </template>
 
 <script>
-import { gridSwitch } from './../plugins/GridSwitch.js'
+import Header from '~/components/Header.vue'
+import Footer from '~/components/Footer.vue'
+import Aside from '~/components/Aside.vue'
+import { gridSwitch } from './../plugins/GridSwitch.js';
+import { tabsJS } from './../plugins/tabs.js'
 
 export default {
+  components: {
+    Header,
+    Footer,
+    Aside
+  },
+  data(){
+    return {
+      products: [],
+      categories:  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+      catidadProducto: 0
+    }
+  },
   async asyncData({ $woocomerceApi }) {
     try {
-      const { data } = await $woocomerceApi.get('products')
-
+      const products = await $woocomerceApi.get('products');
+      /*const categories = await $woocomerceApi.get('products/categories');*/
       return {
-        data
+        products: products.data,
+        /*categories: categories.data*/
       }
     } catch (err) {
       console.log(err)
     }
   },
   mounted() {
-    gridSwitch()
+    gridSwitch();
+    tabsJS();
   },
   destroyed() { // remove the JS code once the component has been destroyed
-    gridSwitch()
+    gridSwitch(this.products)
+  },
+
+  methods: {
+    sumarProducto() {
+     this.catidadProducto ++;
+     
+      
+    },
+     restarProducto() {
+      this.catidadProducto--
+      
+    }
   }
 }
 </script>
 
 <style lang="scss">
+.card-v10__label {
+  font-size: var(--text-sm);
+  font-weight: bold;
+}
+.card-v10__social-item {
+  .icon {
+    font-size: 16px;
+  }
+}
+.quanty {
+  display: block;
+  text-align: center;
+}
 .grid-switch {
   .btns {
     --btns-button-radius: var(--radius-md);
